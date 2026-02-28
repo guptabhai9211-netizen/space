31c643f5b19b22288c9d8f73a758e417b484f218
Meena Chaudhary <your-real-email@gmail.com>
2026-02-28 19:35:46 +0530

    Updated gallery page and dependencies

--- src/Components/GaleryPage.jsx
+++ src/Components/GaleryPage.jsx
@@ -1,10 +1,12 @@
 import SpaceGallery from "./Gallery";
+  import ExploreSection from "./Pages/Explore";
 
 
 export default function GalleryPage() {
   return (
     <main style={{ paddingTop: '68px' }}>
       <SpaceGallery/>
+      <ExploreSection/>
     </main>
   )
 }
